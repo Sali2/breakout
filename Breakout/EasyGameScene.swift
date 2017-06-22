2378a034078544908dc79501f5538edf8ddf81fd
Sakina Ali <sali2@sakinas-macbook-air.local>
2017-06-22 10:04:42 -0500

    yo4

--- Breakout/EasyGameScene.swift
+++ Breakout/EasyGameScene.swift
@@ -1,14 +1,14 @@
 //
-//  EasyGameScene.swift
+//  GameScene.swift
 //  Breakout
-//n
-//  Created by Devanshi Chakrabarti on 6/22/17.
-//  Copyright © 2017 Sakina Ali. All rights reserved.
 //
+//  Created by Sakina Ali on 6/21/17.
+//  Copyright © 2017 Sakina Ali. All rights reserved.
+// this is a test uhggg
 
-import UIKit
 import SpriteKit
-
+import GameplayKit
+//Add to use contact physics
 class EasyGameScene: SKScene, SKPhysicsContactDelegate
 {
     var ball : SKSpriteNode!
@@ -28,6 +28,7 @@ class EasyGameScene: SKScene, SKPhysicsContactDelegate
         concieveBrick()
         concieveBrickTwo()
         concieveBrickThree()
+        print(numOfBrick)
         
     }
     
@@ -55,12 +56,50 @@ class EasyGameScene: SKScene, SKPhysicsContactDelegate
         }
     }
     
-    
-    
+    func didBegin(_ contact: SKPhysicsContact)
+    {
+        
+        print("contact func")
+        for i in bricksArray
+        {
+            if (contact.bodyA.node! == ball) && (contact.bodyB.node! ==  i) || (contact.bodyA.node! == i) && (contact.bodyB.node! == ball)
+            {
+                print("Brick Hit")
+                i!.removeFromParent()
+                numOfBrick -= 1
+            }
+            print(numOfBrick)
+            if numOfBrick == 0
+            {
+                ball.physicsBody?.isDynamic = false
+                ball.position = CGPoint(x: frame.midX, y: frame.midY)
+                //   let addAlert = UIAlertController(title: "You Won!!!", message: "Congrats, you finished the level!!!", preferredStyle: .alert)
+                //    let addButtonTapped = UIAlertAction(title: "Next Level", style: .default)
+                //   addAlert.addAction(addButtonTapped)
+                //   addAlert.addAction(UIAlertAction(title: "Play Again", style: .cancel, handler: nil))
+            }
+        }
+        
+        if contact.bodyA.node == loseZone || contact.bodyB.node == loseZone
+        {
+            print("Ball lost!")
+            ball.physicsBody?.isDynamic = false
+            ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 5))
+            ballCount -= 1
+            if ballCount <= 0
+            {
+                print("you lose")
+                //     let myAlert = UIAlertController(title: "Good Try", message: "Sorry, you lost! Try again!", preferredStyle: UIAlertControllerStyle.alert)
+                //      let resetButton = UIAlertAction(title: "Play Again", style: UIAlertActionStyle.default, handler: {sender in))
+                print("lord jesus, i dont wanna die")
+                
+            }
+        }
+    }
     
     func createBackground()
     {
-        let stars = SKTexture(imageNamed: "stars3")
+        let stars = SKTexture(imageNamed: "Deep Blue Space iPhone 5 Wallpaper")
         for i in 0...1                          //Creates 2 stars for seamless transtition
         {
             let starsBackground = SKSpriteNode(texture: stars)
@@ -113,16 +152,17 @@ class EasyGameScene: SKScene, SKPhysicsContactDelegate
         addChild(paddle)
     }
     
-    func concieveBrickThree()
+    func concieveBrick()
     {
         var xPos = frame.minX + 45
-        var yPos = frame.maxY - 30
+        var yPos = frame.maxY - 130
         
-        for i in 0...14
+        for i in 0...4
         {
             print("part 1")
             
-            var i = SKSpriteNode(color: UIColor.cyan, size: CGSize(width: frame.width / 6, height: frame.height / 24.5))
+            
+            var i = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width / 6, height: frame.height / 24.5))
             i.name = "i"
             i.physicsBody = SKPhysicsBody(rectangleOf: i.size)
             i.physicsBody?.isDynamic = false
@@ -141,7 +181,7 @@ class EasyGameScene: SKScene, SKPhysicsContactDelegate
         var xPos = frame.minX + 45
         var yPos = frame.maxY - 80
         
-        for i in 0...14
+        for i in 5...9
         {
             print("part 1")
             
@@ -159,17 +199,17 @@ class EasyGameScene: SKScene, SKPhysicsContactDelegate
         }
     }
     
-    func concieveBrick()
+    func concieveBrickThree()
     {
         var xPos = frame.minX + 45
-        var yPos = frame.maxY - 130
+        var yPos = frame.maxY - 30
         
-        for i in 0...14
+        for i in 10...14
         {
             print("part 1")
             
-            var i = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width / 6, height: frame.height / 24.5))
-            i.name = "Brick"
+            var i = SKSpriteNode(color: UIColor.cyan, size: CGSize(width: frame.width / 6, height: frame.height / 24.5))
+            //  bricksArray:[0] = "Brick1"
             i.physicsBody = SKPhysicsBody(rectangleOf: i.size)
             i.physicsBody?.isDynamic = false
             i.position = CGPoint(x: xPos, y: yPos)
@@ -184,7 +224,7 @@ class EasyGameScene: SKScene, SKPhysicsContactDelegate
     
     func constructLoseZone()
     {
-        loseZone = SKSpriteNode(color: UIColor.clear, size: CGSize(width: frame.width, height: 50))
+        loseZone = SKSpriteNode(color: UIColor.blue, size: CGSize(width: frame.width, height: 50))
         loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
         loseZone.name = "Lose Zone"
         loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
